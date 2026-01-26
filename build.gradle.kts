b86ec6ba43b2e6559af87de52665892cd064ef0d
nostalfinals <yuu8583@proton.me>
2026-01-26 20:19:52 +0800

    chore: init repository

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "club.plutoproject.charonflow"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    
+}
+
+kotlin {
+    jvmToolchain(21)
+}
